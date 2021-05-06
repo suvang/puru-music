8ba14212bf049d0fbdf2b87675ab13f7e22bdd0b
Suvang <suvang08@gmail.com>
2021-05-07 03:56:00 +0530

    updated menu items to navigate with withrouter hoc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
-import Homepage from './pages/homepage/homepage.component';
+import {Switch, Route} from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+
+ const HatsPage = () => {
+
+  return (
+    <div>
+    <h1>HATS PAGE</h1>
+  </div>
+  )
+  
+ }
 
 function App() {
   return (
     <div >
-     <Homepage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/> 
+        <Route exact path='/hats' component={HatsPage}/>   
+      </Switch>  
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Directory from '../../components/directory/directory.component'
 import './homepage.styles.scss'
 
-const Homepage = () => {
+const HomePage = () => {
     return (
         <div className="homepage">
             <Directory />
@@ -10,5 +10,5 @@ const Homepage = () => {
     )
 }
 
-export default Homepage
+export default HomePage
 
