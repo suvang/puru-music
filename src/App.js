a507ca1977dc9d234c0ab46e8b6694a210d1fc70
Suvang <suvang08@gmail.com>
2021-05-15 17:16:29 +0530

    finsihed sign in handle submit method

--- src/App.js
+++ src/App.js
@@ -34,7 +34,9 @@ class App extends React.Component {
             ...snapShot.data()
             }
           })
+         
         })
+
       }else{
         this.setState({currentUser: userAuth})
       }
