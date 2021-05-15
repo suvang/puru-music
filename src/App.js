cffa6e286305ef4562b1795a4c64b9c2f1aa8571
Suvang <suvang08@gmail.com>
2021-05-16 02:26:31 +0530

    updated redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux'
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -30,7 +30,7 @@ class App extends React.Component {
           })
          
         })
-      
+       
       }
         setCurrentUser(userAuth)
        
@@ -50,7 +50,11 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}/> 
           <Route exact path='/shop' component={ShopPage}/>   
-          <Route exact path='/signin' component={SignInAndSignUpPage}/> 
+          <Route exact path='/signin' render={() => this.props.currentUser? (
+            <Redirect to='/'/>
+          ) : (
+            <SignInAndSignUpPage />
+          )}/> 
         </Switch>  
       </div>
     );
@@ -58,8 +62,12 @@ class App extends React.Component {
 
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
