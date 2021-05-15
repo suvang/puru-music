c59488bd4a3008f3376cdfe4f595440c3fbb8184
Suvang <suvang08@gmail.com>
2021-05-15 16:59:10 +0530

    implemented firebase user in database and cerated signup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import React from 'react';
 
 class App extends React.Component {
@@ -17,16 +17,31 @@ class App extends React.Component {
     }
   }
 
+ 
+
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+            id: snapShot.id,
+            ...snapShot.data()
+            }
+          })
+        })
+      }else{
+        this.setState({currentUser: userAuth})
+      }
     })
+    
   }
-
+  
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
