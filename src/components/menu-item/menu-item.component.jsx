2dbc9f5279ceba0236e5ca85fcd2e4213cb63924
Suvang <suvang08@gmail.com>
2021-05-27 21:43:21 +0530

    added cart reducer and toggle cart

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,7 +4,6 @@ import './menu-item.styles.scss'
 
 const MenuItem= ({title, imageUrl, size, history, linkUrl,match}) => {
 
-    console.log(match)
     return (
     <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}}  />
